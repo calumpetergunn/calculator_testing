1b9bf3c91fd05392c642d613a52f169bd32f87d7
calumpetergunn <calumgunn@gmail.com>
2021-04-07 21:12:45 +0100

    unittests all passing

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -19,8 +19,43 @@ describe('App.vue', () => {
   })
 
   it('can subtract', () => {
-    wrapper.vm.runningTotal = 7
+    wrapper.vm.previousTotal = 7
     wrapper.vm.subtract('4')
     expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+
+  it('can multiply', () => {
+    wrapper.vm.previousTotal = 3
+    wrapper.vm.multiply('5')
+    expect(wrapper.vm.runningTotal).to.equal(15)
+  })
+
+  it('can divide', () => {
+    wrapper.vm.previousTotal = 21
+    wrapper.vm.divide('7')
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+
+  it('can number click', () => {
+    wrapper.vm.newTotal = false
+    wrapper.vm.runningTotal = 2
+    wrapper.vm.numberClick('4')
+    expect(wrapper.vm.runningTotal).to.equal(24)
+  })
+
+  it('can operator click', () => {
+    wrapper.vm.previousTotal = 10
+    wrapper.vm.previousOperator = '*'
+    wrapper.vm.add('5')
+    expect(wrapper.vm.runningTotal).to.equal(15)
+  })
+
+  it('can clear click', () => {
+    wrapper.vm.runningTotal = 15
+    wrapper.vm.previousTotal = 32
+    wrapper.vm.clearClick()
+    expect(wrapper.vm.runningTotal).to.equal(0)
+    wrapper.vm.multiply(2)
+    expect(wrapper.vm.runningTotal).to.equal(64)
   })
 })
