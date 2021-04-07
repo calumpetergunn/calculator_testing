86a4fce002b6a04d2d15d8504c58f26926e63b8c
calumpetergunn <calumgunn@gmail.com>
2021-04-07 21:58:09 +0100

    added division by zero

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -24,4 +24,67 @@ describe('calculator', () => {
     cy.get('#operator_equals').click();
     cy.get('.display').should('contain', '17')
   })
+
+  it(' should chain operations together', () => {
+    cy.get('#number7').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '5764801')
+  })
+
+  it('should handle negative numbers', () => {
+    cy.get('#number8').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number1').click();
+    cy.get('#number6').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-8')
+    cy.get('#operator_multiply').click();
+    cy.get('#number8').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-640')
+  })
+
+  it('should handle decimal numbers', () => {
+    cy.get('#number6').click();
+    cy.get('#number8').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number9').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '7.555555555555555')
+  })
+
+  it('should handle large numbers', () => {
+    cy.get('#number9').click();
+    cy.get('#number8').click();
+    cy.get('#number9').click();
+    cy.get('#number8').click();
+    cy.get('#number9').click();
+    cy.get('#number8').click();
+    cy.get('#number9').click();
+    cy.get('#number8').click();
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '21997755091133560');
+  })
+
+  it('should handle division by zero', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', 'error')
+  })
 })
