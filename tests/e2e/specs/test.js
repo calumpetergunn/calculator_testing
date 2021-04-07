c524b537d6b67daf943c8d49196da71eca07599f
calumpetergunn <calumgunn@gmail.com>
2021-04-07 21:20:43 +0100

    added first three e2e tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -9,4 +9,19 @@ describe('calculator', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should display running total', () => {
+    cy.get('#number3').click();
+    cy.get('#number4').click();
+    cy.get('.display').should('contain', '34')
+  })
+
+  it('should have working mathematical operations', () => {
+    cy.get('#number3').click();
+    cy.get('#number4').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '17')
+  })
 })
